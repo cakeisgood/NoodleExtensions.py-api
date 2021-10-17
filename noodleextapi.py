ab8d0a84b36456207d65fd4e90ef16ecd0e28aea
no-u <81192508+no-u101@users.noreply.github.com>
2021-10-16 21:31:18 -0700

    NoodleExtensions-api.py

---
+++ noodleextapi.py
@@ -0,0 +1,45 @@
+import random
+
+
+
+
+maps_rand_list=("https://beatsaver.com/maps/1d151","https://beatsaver.com/maps/1a9d6","https://beatsaver.com/maps/1c744")
+
+
+class get:
+    mappers=("Mawntee CyanSnow Reaxt Sands Reddek SuperMemer417 Jevk 𝓼𝓹𝓸𝓸𝓴𝔂-le-monke Benevolence ProtoPhonix TzurS11 Discovered")
+
+    mappers_random_list=("Mawntee","CyanSnow","Reaxt","Sands","Reddek","SuperMemer417","Jevk","𝓼𝓹𝓸𝓸𝓴𝔂 le monke","Benevolence","ProtoPhonix","TzurS11","Discovered")
+    random_mapper=(random.choice(mappers_random_list))
+    
+    noodle_maps=()
+
+    class maps:
+        random_noodle_map=()
+
+
+class github:
+
+
+
+    class noodle:
+        latest=("https://github.com/Aeroluna/NoodleExtensions/releases/latest")
+
+
+class NoodleExtensions:
+    class com:
+        class api:
+            mapper_list=("Mawntee CyanSnow Reaxt Sands Reddek SuperMemer417 Jevk 𝓼𝓹𝓸𝓸𝓴𝔂-le-monke Benevolence ProtoPhonix TzurS11 Discovered")
+
+
+class discord_noodle:
+    class noodleapi:     
+        def rand_mapper():
+            noodle_com_api_rand_mapper_list=("Mawntee","CyanSnow","Reaxt","Sands","Reddek","SuperMemer417","Jevk","𝓼𝓹𝓸𝓸𝓴𝔂 le monke","Benevolence","ProtoPhonix","TzurS11","Discovered")
+            noodle_com_api_rand_mapper=(random.choice(noodle_com_api_rand_mapper_list))
+            return (noodle_com_api_rand_mapper)
+
+        def rand_map():
+            noodle_com_api_rand_map_list=(maps_rand_list)
+            noodle_com_api_rand_map=(random.choice(noodle_com_api_rand_map_list))
+            return (noodle_com_api_rand_map)
